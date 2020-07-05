01ae5c3896616f0d87818cf2eaca01998e36ee18
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-07-06 03:50:05 +0530

    finalized imports

--- storesites.py
+++ storesites.py
@@ -1,6 +1,8 @@
 import sys
 import datetime
 import pathlib #pip install pathlib
+import lz4.block #pip install lz4
+import json
 import webbrowser #comes bundled with python 3.6
 
 class OpenLinks:
@@ -27,6 +29,65 @@ class SaveLinks:
 				print("URL: ", url, "\n")
 				file.write(url+'\n\n') 
 		#file.close() 
+	
+
+	def get_urls(self):
+		path = pathlib.Path.home().joinpath('.mozilla/firefox')
+		files = path.glob('*default*/sessionstore-backups/recovery.js*')
+
+		try:
+			template = sys.argv[1]
+		except IndexError:
+			template = '%s %s'
+
+		urls=[] #empty list where urls will be stored
+		for f in files:
+			b = f.read_bytes()
+			if b[:8] == b'mozLz40\0':
+				b = lz4.block.decompress(b[8:])
+			j = json.loads(b)
+			for w in j['windows']:
+				for t in w['tabs']:
+					i = t['index'] - 1 
+					#print(template % ('Url:' , t['entries'][i]['url']))
+					urls.append(str(t['entries'][i]['url']))
+				
+		return urls
+						
+				
+				
+def main(): #driver code
+
+	savelink=SaveLinks()
+	openlink=OpenLinks()
+	
+	flag=0
+	
+	while flag!=-1:
+		choice=int(input('1.Save Links to file?\n2.Open Saved links in a browser window\nPress -1 to Exit\n\nEnter your choice:'))
+		flag1=True
+		
+		if choice==1:
+			savelink.append_urls_to_file()
+			
+		elif choice==2:
+			fname = input('Enter the filename')
+			
+			try:
+				 f = open(fname)
+				 f.close()
+			except FileNotFoundError:
+				 print('File does not exist')
+				 flag1=False
+
+			if flag1==True :
+				openlink.open_urls_in_browser(fname)#provide path to the file
+				
+		elif choice==-1:
+			flag=choice;
+			
+		else :
+			print('Invalid choice. Try again.\n\n')
 	
 	
 if __name__ == "__main__":
